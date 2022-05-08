5a11c46d6ee24f5f402193d7abffc9700caab65d
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-05-08 18:53:48 +0600

    jwt social login use

--- src/components/Allproduct/HomeProductCard/HomeProductCard.js
+++ src/components/Allproduct/HomeProductCard/HomeProductCard.js
@@ -7,7 +7,28 @@ const HomeProductCard = ({product}) => {
     const { productname, description, img ,_id ,price,providername} = product;
     return (
         <>
-            
+            <Col lg={4} md={6}>
+                <Card className='m-2' >
+                    <Card.Img variant="top" src={img} className="img-fluid " />
+                    <Card.Body>
+                        <Card.Title>{productname}</Card.Title>
+                        <Card.Text>
+                          <b>Provider Name : </b>  {providername} 
+                        </Card.Text>
+                        <Card.Text>
+                        <b>Description:</b> {description} 
+                        </Card.Text>
+                        <Card.Text>
+                        <b>Price : </b>  {price} 
+                        </Card.Text>
+                        <span className='d-flex justify-content-between  flex-column '>
+                        {/* <Button variant="outline-warning mb-1" onClick={() => navigate(`/checkout/${_id}`)}>Order Now</Button> */}
+                        <Button variant="outline-dark mt-1" onClick={() => navigate(`/Allproduct/${_id}`)}>Details</Button>
+                        </span>
+                  
+                    </Card.Body>
+                </Card>
+            </Col>
         </>
     );
 };
